b6c4681f94e0cca611bea803acd5030e7dd83753
suminhan123 <suminh324@gmail.com>
2024-11-02 14:39:06 +0900

    feat: 업로드된 pdf 파일 데이터 파일 읽어오기

--- src/components/PdfViewer/index.jsx
+++ src/components/PdfViewer/index.jsx
@@ -1,4 +1,7 @@
 import * as pdfjsLib from "pdfjs-dist";
+import { useCallback, useEffect, useRef, useState } from "react";
+import { getExtractedText } from "../../utils/getExtractedText";
+import { getParseTable } from "../../utils/getParseTable";
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
@@ -6,10 +9,26 @@ pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
 ).toString();
 
 const PdfViewer = ({ pdfFile }) => {
-  return (
-    <div>
-      <button>pdf 뷰어</button>
-    </div>
-  );
+  useEffect(() => {
+    if (pdfFile) {
+      const table = handlePdfUpload(pdfFile);
+      console.log(table);
+    }
+  }, [pdfFile]);
+
+  const handlePdfUpload = async (file) => {
+    const fileReader = new FileReader();
+    fileReader.onload = async (event) => {
+      const typedarray = new Uint8Array(event.target.result);
+      const loadingTask = pdfjsLib.getDocument(typedarray);
+
+      const doc = await loadingTask.promise;
+      const page = await doc.getPage(6);
+    };
+
+    fileReader.readAsArrayBuffer(file);
+  };
+
+  return <div></div>;
 };
 export default PdfViewer;
