f4a07fc4249bf6047e4c3f24f868b4f9ebff84e2
suminhan123 <suminh324@gmail.com>
2024-11-02 13:17:29 +0900

    feat: pdf 파일 업로드 하는 input 컴포넌트 생성

---
+++ src/components/PdfUploader/index.jsx
@@ -0,0 +1,21 @@
+import "./PdfUploader.css";
+
+const PdfUploader = ({ setPdfFile }) => {
+  const onChange = (event) => {
+    const file = event.target.files[0];
+    if (file && file.type === "application/pdf") {
+      setPdfFile(file);
+    } else {
+      alert("PDF 파일만 업로드 가능합니다.");
+    }
+  };
+  return (
+    <input
+      className="pdfUploader"
+      type="file"
+      onChange={onChange}
+      accept=".pdf"
+    />
+  );
+};
+export default PdfUploader;
