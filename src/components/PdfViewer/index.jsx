263ed346972a1a789b623775a52a00b8bc9209d3
suminhan123 <suminh324@gmail.com>
2024-11-02 17:28:44 +0900

    design: PdfView의 스타일 적용

--- src/components/PdfViewer/index.jsx
+++ src/components/PdfViewer/index.jsx
@@ -1,5 +1,5 @@
 import * as pdfjsLib from "pdfjs-dist";
-
+import "./index.css";
 import { usePdfTexts } from "../../hooks/usePdfTexts";
 
 pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
@@ -11,28 +11,34 @@ const PdfViewer = ({ pdfFile }) => {
   const table = usePdfTexts(pdfFile);
 
   return (
-    <>
+    <div className="container">
       {table.length > 0 && (
-        <table>
-          <caption>신 구조문 대비</caption>
-          <thead>
-            <tr>
-              <th>현행</th>
-              <th>개정안</th>
-            </tr>
-          </thead>
-          <tbody>
-            {table.map((row, idx) => (
-              <tr key={idx}>
-                {row.map((v, i) => (
-                  <td key={i}>{v}</td>
-                ))}
-              </tr>
-            ))}
-          </tbody>
-        </table>
+        <>
+          <h1 className="title">신 구조문 대비</h1>
+          <div className="table">
+            <div className="section">
+              <div className="th-1">
+                <h4>현행</h4>
+              </div>
+              <div className="th-2">
+                <h4>개정안</h4>
+              </div>
+            </div>
+            <div className="body">
+              {table.map((row, idx) => (
+                <div className="content" key={idx}>
+                  {row.map((v, i) => (
+                    <div key={i}>
+                      <h5>{v}</h5>
+                    </div>
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+        </>
       )}
-    </>
+    </div>
   );
 };
 export default PdfViewer;
