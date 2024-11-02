fe4950cf7b06e86385c8f23fcf45f15820b56c5a
suminhan123 <suminh324@gmail.com>
2024-11-02 15:26:20 +0900

    feat: 파싱한 텍스트 table 생성

--- src/components/PdfViewer/index.jsx
+++ src/components/PdfViewer/index.jsx
@@ -10,6 +10,29 @@ pdfjsLib.GlobalWorkerOptions.workerSrc = new URL(
 const PdfViewer = ({ pdfFile }) => {
   const table = usePdfTexts(pdfFile);
 
-  return <div></div>;
+  return (
+    <>
+      {table.length > 0 && (
+        <table>
+          <caption>신 구조문 대비</caption>
+          <thead>
+            <tr>
+              <th>현행</th>
+              <th>개정안</th>
+            </tr>
+          </thead>
+          <tbody>
+            {table.map((row, idx) => (
+              <tr key={idx}>
+                {row.map((v, i) => (
+                  <td key={i}>{v}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      )}
+    </>
+  );
 };
 export default PdfViewer;
